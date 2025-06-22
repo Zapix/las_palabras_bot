0eccbacfe8b07dafc1cdf9dba479f71de46aca4b
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2025-06-22 17:31:46 +0200

    Initial project setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer};
+
+use las_palabras_bot::api::health;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(|| async { "Hello, World!" }))
+            .route("/health", web::get().to(health))
+        // Here you can add your routes, middleware, etc.
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await?;
+
+    Ok(())
 }
