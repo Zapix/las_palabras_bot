100f047c989eb696212abd102591a09d5281d619
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2025-08-31 20:26:42 +0200

    8 add route to create word

---
+++ src/api/vocabulary/create.rs
@@ -0,0 +1,17 @@
+use crate::domain::vocabulary::raw_word::RawWord;
+use crate::domain::vocabulary::repository::{VocabularyDb, VocabularyTrait};
+use actix_web::{HttpResponse, Responder, error, web};
+use sqlx::PgPool;
+
+#[tracing::instrument(name = "create_word", skip(db_pool))]
+pub async fn create_word(
+    db_pool: web::Data<PgPool>,
+    raw_word: web::Json<RawWord>,
+) -> Result<impl Responder, actix_web::Error> {
+    let vocabulary_repo = VocabularyDb::new(db_pool.as_ref());
+    let word = vocabulary_repo
+        .create_word(raw_word.into_inner())
+        .await
+        .map_err(|_e| error::ErrorInternalServerError("Cannot create word in db"))?;
+    Ok(HttpResponse::Created().json(word))
+}
