03f01ba4a51a1a04e006a636e4b8fe62a43b4d28
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2025-09-03 23:40:08 +0200

    32 implement filters for vocabulary list

--- src/api/vocabulary/list.rs
+++ src/api/vocabulary/list.rs
@@ -5,10 +5,16 @@ use tracing_attributes::instrument;
 use crate::api::pagination::{DEFAULT_PAGE, PageQuery, Pagination};
 use crate::domain::vocabulary::repository::{VocabularyDb, VocabularyTrait};
 
+#[derive(serde::Deserialize, Debug)]
+pub struct FilterQuery {
+    pub is_verified: Option<bool>,
+}
+
 #[instrument(name = "list_words", skip(db_pool), err)]
 pub async fn list_words(
     db_pool: web::Data<PgPool>,
     page_query: web::Query<PageQuery>,
+    filter_query: web::Query<FilterQuery>,
 ) -> Result<impl Responder, Error> {
     let page = page_query.page.unwrap_or(0);
     let per_page = page_query.per_page.unwrap_or(DEFAULT_PAGE);
@@ -20,7 +26,7 @@ pub async fn list_words(
         .map_err(|_e| error::ErrorInternalServerError("Cannot get words count from db"))?;
 
     let items = vocabulary_repo
-        .list_word(page, per_page)
+        .list_word(page, per_page, filter_query.is_verified.into())
         .await
         .map_err(|_e| error::ErrorInternalServerError("Cannot get words from db"))?;
 
